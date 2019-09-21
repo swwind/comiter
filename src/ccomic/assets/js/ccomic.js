3b7cdef6a1e2655d4a473b19037b293251b6df7b
swwind <swwind233@gmail.com>
2019-09-21 20:51:08 +0800

    improve ccomic

--- src/ccomic/assets/js/ccomic.js
+++ src/ccomic/assets/js/ccomic.js
@@ -3,10 +3,26 @@ console.log($data);
 const container = document.querySelector('.container');
 
 const rowMode = () => {
-  let now = 0;
+  let now = 0, nowpage = 0;
+  const imgs = Array.from(document.querySelectorAll('.container img'));
+  const prefixOfImgs = (length) => {
+    return imgs.slice(0, length)
+      .map((el) => el.clientWidth)
+      .reduce((a, b) => a + b, 0);
+  }
+
+  const nowpageElem = document.getElementById('nowpage');
+
   const makeMove = (delta) => {
-    container.style.transform = `translateX(${now -= delta}px)`;
-    console.log(delta);
+    now -= delta;
+    container.style.transform = `translateX(${now}px)`;
+    for (let i = 0; i < imgs.length; ++ i) {
+      if (prefixOfImgs(i) >= -now) {
+        nowpage = i;
+        break;
+      }
+    }
+    nowpageElem.textContent = String(nowpage + 1);
   }
   document.addEventListener('mousewheel', (e) => {
     if (!e.deltaX) {
@@ -23,6 +39,13 @@ const rowMode = () => {
   document.addEventListener('touchmove', (e) => {
     makeMove(touchpos - e.changedTouches[0].clientX);
     touchpos = e.changedTouches[0].clientX;
+  });
+
+  document.querySelector('.leftpage').addEventListener('click', (e) => {
+    makeMove(now + prefixOfImgs(Math.max(0, nowpage - 1)));
+  });
+  document.querySelector('.rightpage').addEventListener('click', (e) => {
+    makeMove(now + prefixOfImgs(Math.min(imgs.length - 1, nowpage + 1)));
   });
 }
 
